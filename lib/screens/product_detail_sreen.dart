b617b8b4220aaee56db0cda8e50be5c688815156
Ildar <shakirov.i@gmail.com>
2021-03-16 17:01:07 +0200

    product details screen

--- lib/screens/product_detail_sreen.dart
+++ lib/screens/product_detail_sreen.dart
@@ -13,9 +13,37 @@ class ProductDetailScreen extends StatelessWidget {
         Provider.of<ProductsProvider>(context, listen: false).findById(id);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(item.title),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(item.title),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.infinity,
+                child: Image.network(
+                  item.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              SizedBox(height: 10),
+              Text(
+                '\$ ${item.price}',
+                style: TextStyle(color: Colors.grey, fontSize: 20),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Text(
+                  item.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
