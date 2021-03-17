374a67a67a464a9a9a44fbc63c117b485c7e1147
Ildar <shakirov.i@gmail.com>
2021-03-17 19:25:14 +0200

    added edit project screen & edit form

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+
+  EditProductScreen({Key key}) : super(key: key);
+
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  final _priceFocusNode = FocusNode();
+  final _desciptionFocusNode = FocusNode();
+
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _desciptionFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Edit product')),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.multiline,
+                focusNode: _desciptionFocusNode,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/user_product_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/edit_product_screen.dart';
+import '../providers/products.dart';
+import '../widgets/user_product_item.dart';
+import '../widgets/app_drawer.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<ProductsProvider>(context);
+
+    return Scaffold(
+      drawer: AppDrawer(),
+      appBar: AppBar(
+        // backgroundColor: Theme.of(context).primaryColor,
+        title: Text('Your products'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                EditProductScreen.routeName,
+              );
+            },
+            icon: Icon(Icons.add),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(8),
+        child: ListView.builder(
+            itemCount: productsData.items.length,
+            itemBuilder: (_, i) {
+              return Column(
+                children: [
+                  UserProduct(
+                    imageUrl: productsData.items[i].imageUrl,
+                    title: productsData.items[i].title,
+                  ),
+                  Divider(),
+                ],
+              );
+            }),
+      ),
+    );
+  }
+}
